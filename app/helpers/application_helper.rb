8ea5dca0cc6a733c4ac8ca9c0fdf1aedf0d6049d
François Catuhe <francois@hey.com>
2026-07-19 14:18:39 +0200

    Web page metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,39 @@ module ApplicationHelper
   def render_erb(content)
     render inline: content, layout: false
   end
+
+  def site_name
+    "Skyblip"
+  end
+
+  def title
+    [ @page&.title.presence, site_name ].uniq.compact.join(" · ")
+  end
+
+  def description
+    @page&.description
+  end
+
+  def author
+    "Skyblip"
+  end
+
+  def canonical_url
+    url_for(only_path: false)
+  end
+
+  def robots_content
+    Rails.env.production? ? "index, follow" : "noindex, nofollow"
+  end
+
+  def og_meta_tag(key, content)
+    case key
+    when :title, :description, :image
+      tag(:meta, name: key, property: "og:#{key}", content: content)
+    when :author
+      tag(:meta, name: key, content: content)
+    else
+      tag(:meta, property: "og:#{key}", content: content)
+    end
+  end
 end
