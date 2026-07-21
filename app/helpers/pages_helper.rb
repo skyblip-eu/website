b45be415ec329215a1a85e01da5d5ec13f4c4705
François Catuhe <francois@hey.com>
2026-07-22 00:17:24 +0200

    Add site navigation bar
    - _nav partial: wordmark link home, localized page links, locale
      switcher folded in (no longer absolutely positioned)
    - link_to_page accepts a label: option for short nav labels
    - nav labels in config/locales (fr/en)

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,10 +1,10 @@
 module PagesHelper
-  def link_to_page(slug, html_options = {})
+  def link_to_page(slug, label: nil, **html_options)
     page = Page.find_by(base_slug: slug) || Page.find_by(base_slug: slug, locale: I18n.default_locale)
     raise Decant::FileNotFound, "Couldn't find Page with 'base_slug'=#{slug} and 'locale'=#{I18n.locale}" unless page
 
     lang = page.locale unless page.locale == I18n.locale
-    link_to page.title, page_url_for(page, only_path: true), { lang: }.merge(html_options)
+    link_to label || page.title, page_url_for(page, only_path: true), { lang: }.merge(html_options)
   end
 
   def pages_image_tag(path, options = {})
